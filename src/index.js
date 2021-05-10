88e80ba7b09d60104b24f5c962a44d2eee4778d9
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-05-11 02:04:56 +0600

    Added endpoint and updated workflow

--- src/index.js
+++ src/index.js
@@ -8,4 +8,14 @@ app.get("/", (req, res) => {
   res.send({ status: "Web server is up and running" });
 });
 
+app.get("/data", (req, res) => {
+  res.send({
+    data: [
+      { name: "Saiful", age: 32 },
+      { name: "Arif", age: 28 },
+      { name: "Sharif", age: 25 },
+    ],
+  });
+});
+
 app.listen(port, () => console.log(`Webserver is listening on port ${port}.`));
